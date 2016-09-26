c67bdff4b036feca92b82cb8426a7f0573e79359
ygandy <yygandy@gmail.com>
2016-09-26 15:13:53 -0400

    submit correct version of batter.app

--- BatterApp.java
+++ BatterApp.java
@@ -1,32 +1,21 @@
-import java.util.ArrayList;
 
-public class BatterApp {
+import java.util.Scanner;
 
-	
-	public static void main(String[] args) {
-		Batter batter = new Batter("Charles");
-		batter.noAtBats.add(0);
-		batter.noAtBats.add(1);// 1/2 = .500
-	
-	System.out.println(batter.calculateBattingAverage());
-//
-//		ArrayList<NoOfAtBats> list = new ArrayList<Integer>();
-//		list.add(new Movie("Frozen", "animation"));
-//		list.add(new Movie("Wall-e", "animation"));
-//		list.add(new Movie("Ice Age", "animation"));
-//		list.add(new Movie("Concussion", "drama"));
-//		list.add(new Movie("Pursuit of Happiness", "drama"));
-//		list.add(new Movie("The Ring", "horror"));
-//		list.add(new Movie("Conjuring", "horror"));
-//		list.add(new Movie("Texas Chainsaw", "horror"));
-//		list.add(new Movie("Bridesmaid", "comedy"));
-//		list.add(new Movie("Coming to America", "comedy"));
+	public class BatterApp {
+	public static class AtBat {
 
-	}
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        double totalBasesEarned = 0;
+	        double totalAtBats = 0;
+	        int  AtBat = 0;
+	        
+	        String message = " ";
+	    }
+	        AtBat bat = new AtBat();{
+	        System.out.print("Welcome to the Batting Average Calculator");
+	    System.out.println("");}
+	        // TODO Auto-generated method stub
 
-	/* private static char[] calculateBattingAverage() {
-		// TODO Auto-generated method stub
-		return null;
-	}*/
-
+	    }
 }
